a4e6a23eb1b64a9741067170b641712a7cad21e1
dmsvk01 <62129820+dmsvk01@users.noreply.github.com>
2020-08-13 20:51:42 +0900

    Create crud.sql

---
+++ crud.sql
@@ -0,0 +1,37 @@
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `test`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `register`
+--
+
+CREATE TABLE IF NOT EXISTS `register` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `sex` varchar(100) NOT NULL,
+  `country` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=21 ;
+
+--
+-- Dumping data for table `register`
+--
+
+INSERT INTO `register` (`id`, `name`, `password`, `email`, `sex`, `country`) VALUES
+(17, 'arun', 'kumar', 'arun@gmail.com', 'Male', 'India'),
+(19, 'sonoo', 'jaiswal', 'sonoo@javatpoint.com', 'male', 'India'),
+(20, 'Ashok', 'ashok', 'ashok@javatpoint.com', 'male', 'India');
